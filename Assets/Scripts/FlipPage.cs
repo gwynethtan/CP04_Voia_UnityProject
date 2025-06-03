d4a7d19efaf57847900d01e2025b86be67ec5689
JacieThoo <s10260236@connect.np.edu.sg>
2025-06-03 23:10:30 +0800

    Updated Book with Pages

--- Assets/Scripts/FlipPage.cs
+++ Assets/Scripts/FlipPage.cs
@@ -6,6 +6,7 @@ public class FlipPage : MonoBehaviour
 {
     public List<Transform> pagePivots;
     public float flipSpeed;
+    public List<float> pageTargetYPositions;
     private int currentPage = 0;
     private bool isFlipping = false;
 
@@ -13,19 +14,22 @@ public class FlipPage : MonoBehaviour
     {
         if (!isFlipping && currentPage < pagePivots.Count)
         {
-            StartCoroutine(FlipBookPage(pagePivots[currentPage]));
+            StartCoroutine(FlipBookPage(pagePivots[currentPage], currentPage));
             currentPage++;
         }
     }
 
-    IEnumerator FlipBookPage(Transform page)
+    IEnumerator FlipBookPage(Transform page, int pageIndex)
     {
         isFlipping = true;
         float currentX = page.localEulerAngles.x;
         float targetX = -180f;
 
         // SO IT DOESNT FLIP WRONG WAY
-        if (currentX > 180) currentX -= 360;
+        if (currentX > 180)
+        {
+            currentX -= 360;
+        }
 
         while (Mathf.Abs(currentX - targetX) > 0.5f)
         {
@@ -35,6 +39,15 @@ public class FlipPage : MonoBehaviour
         }
 
         page.localEulerAngles = new Vector3(targetX, -90, 0);
+
+        // put in corrct order after flipping
+        if (pageIndex < pageTargetYPositions.Count)
+        {
+            Vector3 newPos = page.localPosition;
+            newPos.y = pageTargetYPositions[pageIndex];
+            page.localPosition = newPos;
+        }
+
         isFlipping = false;
     }
 }
