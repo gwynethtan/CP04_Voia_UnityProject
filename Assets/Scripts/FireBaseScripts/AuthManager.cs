43eedf3227d84ff27d76202cdf1f96331e59be72
praiseee <S10255495@connect.np.edu.sg>
2025-07-08 14:44:40 +0800

    Imported Firbase package + Script for AuthManager (Log In - Sign Up)

--- Assets/Scripts/FireBaseScripts/AuthManager.cs
+++ Assets/Scripts/FireBaseScripts/AuthManager.cs
@@ -6,15 +6,15 @@
  */
 
 using TMPro;
-// using Firebase.Auth;
+using Firebase.Auth;
 using UnityEngine;
 using UnityEngine.UI;
-// using Firebase.Extensions;
+using Firebase.Extensions;
 
 public class AuthManager : MonoBehaviour
 {
     public static AuthManager Instance { get; private set; }
-    // private FirebaseAuth auth;
+    private FirebaseAuth auth;
 
     /// <summary>
     /// UI elements for displaying game stats
@@ -30,16 +30,71 @@ public class AuthManager : MonoBehaviour
     /// <summary>
     /// Buttons for various actions
     /// </summary>
+    public Button LogInBtn;
     public Button SignUpBtn;
+    public Button CreateBtn;
+    public Button ForgetBtn;
 
     private void OnLogIn()
     {
-        string email = EmailInput.text.Trim();
+        string username = UsernameInput.text.Trim();
         string password = PasswordInput.text.Trim();
-        LogIn(email, password);
+        LogIn(username, password);
     }
 
-    private void LogIn(string email, string password)
+    private void LogIn(string username, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(username, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.Log($"Account does not exist. Error Logging In: {task.Exception}");
+
+                DisplayErrorMessage("Account does not exist!");
+                return;
+            }
+
+            FirebaseUser user = task.Result.User;
+            Debug.Log($"User logged in successfully");
+
+        });
+    }
+    private void DisplayErrorMessage(string message)
+    {
+        errorMessageText.text = message; // Set the error message text
+        errorMessageText.gameObject.SetActive(true); // Show the error message UI
+
+        // Hide the error message after 1 seconds
+        Invoke("HideErrorMessage", 1f);
+    }
+    private void HideErrorMessage()
+    {
+        errorMessageText.gameObject.SetActive(false); // Hide the error message UI
+    }
+
+    private void OnSignUp()
+    {
+        string email = EmailInput.text.Trim();
+        string username = UsernameInput.text.Trim();
+        string password = PasswordInput.text.Trim();
+
+
+        if (string.IsNullOrEmpty(username))
+        {
+            Debug.Log("Username cannot be empty!");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(password))
+        {
+            Debug.Log("Password cannot be empty!");
+            return;
+        }
+
+        CreateUserAccount(email, password, username);
+    }
+
+    public void CreateUserAccount(string email, string password, string username)
     {
 
     }
